8ca5d74107ec644ebc6e19ac732b5480f609096a
alepskn <alexandraplishkin@gmail.com>
2025-11-26 11:52:28 +0100

    Changed dataset

--- ml-benchmark.py
+++ ml-benchmark.py
@@ -16,22 +16,18 @@ def main():
 
     batch_size = 128
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+    trainset = torchvision.datasets.CIFAR100(root='./data', train=True,
                                             download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                             shuffle=True, num_workers=4)
 
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+    testset = torchvision.datasets.CIFAR100(root='./data', train=False,
                                         download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                             shuffle=False, num_workers=4)
 
-    classes = ('plane', 'car', 'bird', 'cat',
-            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-
     net = models.resnet50(weights=None)
-    net.fc = nn.Linear(net.fc.in_features, 10)
+    net.fc = nn.Linear(net.fc.in_features, 100)
 
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
